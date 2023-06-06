f5cff17a89985e81bd87b558fe646922d53ba5bc
tunglam241 <20521521@gm.uit.edu.vn>
2023-06-06 21:50:42 +0700

    logic page find-tutor

--- components/Find_instructors/Card_info.js
+++ components/Find_instructors/Card_info.js
@@ -8,88 +8,74 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 
 const Card_info = ({ data }) => {
-  console.log(data)
   return (
-    <div>
-      <div
-        className="mt-4 flex flex-col gap-4 rounded-md bg-white p-4 shadow-lg md:flex-row "
-      >
-        <div className="max-h-[200px] w-full md:w-[50%]">
-          <Image
-            src={data.picture}
-            className="h-full w-full rounded-md object-cover"
-            width={200}
-            height={200}
-            alt=""
-          />
-        </div>
+    <Link href={`/find-instructors/${data.email}`}>
+      <div>
+        <div className="mt-4 flex flex-col gap-4 rounded-md bg-white p-4 shadow-lg md:flex-row ">
+          <div className="max-h-[200px] w-full md:w-[50%]">
+            <Image
+              src={data.picture}
+              className="h-full w-full rounded-md object-cover"
+              width={200}
+              height={200}
+              alt=""
+            />
+          </div>
 
-        <div className="col-span-2 w-full flex-col justify-between">
-          <div className=" flex flex-col justify-between md:flex-row">
-            <div className="flex flex-row">
-              <div className="mr-2">
-                <Image
-                  src={data.picture}
-                  className="h-14 w-14 rounded-full"
-                  width={50}
-                  height={50}
-                  alt=""
-                />
+          <div className="col-span-2 w-full flex-col justify-between">
+            <div className=" flex flex-col justify-between md:flex-row">
+              <div className="flex flex-row">
+                <div className="mr-2">
+                  <Image
+                    src={data.picture}
+                    className="h-14 w-14 rounded-full"
+                    width={50}
+                    height={50}
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <div className="flex flex-row items-center gap-4">
+                    <h3>{data.username}</h3>
+                    <span>
+                      <BsCheck2Circle className="h-5 w-5 text-green-600" />
+                    </span>
+                  </div>
+                  <div className="flex flex-row gap-4">
+                    <p className="flex flex-row items-center gap-1">
+                      {/* {data.instructorRating} */}
+                      <span>
+                        {" "}
+                        <AiFillStar className="text-yellow-300" />
+                      </span>
+                      {/* {data.totalReviews}{" "} */}
+                    </p>
+                    <p className="flex flex-row items-center gap-2">
+                      {" "}
+                      <span>
+                        {" "}
+                        <MdOutlinePlace />
+                      </span>
+                      {data.address}, Ok
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div>
-                <div className="flex flex-row items-center gap-4">
-                  <h3>{data.username}</h3>
-                  <span>
-                    <BsCheck2Circle className="h-5 w-5 text-green-600" />
-                  </span>
-                </div>
-                <div className="flex flex-row gap-4">
-                  <p className="flex flex-row items-center gap-1">
-                    {/* {data.instructorRating} */}
-                    <span>
-                      {" "}
-                      <AiFillStar className="text-yellow-300" />
-                    </span>
-                    {/* {data.totalReviews}{" "} */}
-                  </p>
-                  <p className="flex flex-row items-center gap-2">
-                    {" "}
-                    <span>
-                      {" "}
-                      <MdOutlinePlace />
-                    </span>
-                    {data.address}, Ok
-                  </p>
-                </div>
+              <div className="flex flex-row justify-between md:block md:text-end">
+                <p>Starting from:</p>
+                <h3 className="text-xl font-bold text-cyan-400">
+                  ${data.hourlyWage}/hr
+                </h3>
               </div>
             </div>
-            <div className="flex flex-row justify-between md:block md:text-end">
-              <p>Starting from:</p>
-              <h3 className="text-xl font-bold text-cyan-400">
-                ${data.hourlyWage}/hr
-              </h3>
+
+            <div className="mt-4 max-h-20 overflow-y-auto">
+              <p dangerouslySetInnerHTML={{ __html: data.bio }}></p>
             </div>
           </div>
-         
-          <div className="mt-4 max-h-20 overflow-y-auto">
-            <p dangerouslySetInnerHTML={{ __html: data.bio }}></p>
-          </div>
-
-          <div className="mt-auto">
-            <Link href="#" className="hover:opacity-80">
-              <span
-                className={`rounded-md p-2 text-white`}
-                style={{ backgroundColor: data.color }}
-              >
-                {data.subjectName}
-              </span>
-            </Link>
-          </div>
-
-         
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- pages/find-instructors/index.js
+++ pages/find-instructors/index.js
@@ -1,73 +1,64 @@
 import Layout from "../../components/Layout/Layout";
 import Card_info from "../../components/Find_instructors/Card_info";
 import { AiFillStar } from "react-icons/ai";
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import axios from "axios";
-export const FindInstructors = () => {
-  const [allTutor,setAllTutor]=useState([]);
-  const [backupTutor,setBackupTutor]=useState([]);
-  const [search,setSearch]=useState("");
-  const [startPrice,setStartPrice]=useState("");
-  const [endPrice,setEndPrice]=useState("");
-  const [rating1,setRating1]=useState(false);
-  const [rating2,setRating2]=useState(false);
-  const [rating3,setRating3]=useState(false);
-  const [rating4,setRating4]=useState(false);
-  const [rating5,setRating5]=useState(false);
-   useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get(`${process.env.NEXT_PUBLIC_REST_API_ENDPOINT}/users`);
-      const data = res.data;
-      setAllTutor(data.filter((info)=>info.hourlyWage!=0));
-      setBackupTutor(data.filter((info)=>info.hourlyWage!=0));
-    };
-    fetchData();
+export const FindInstructors = ({ instructors }) => {
+  const [allTutor, setAllTutor] = useState([]);
+  const [search, setSearch] = useState("");
+  const [selectRatingChange, setSlectRatingChange] = useState("0");
+  const [selectOption,setSelectOption]=useState("");
+  useEffect(() => {
+    setAllTutor(instructors);
   }, []);
-  useEffect(()=>{
+  useEffect(() => {
     setAllTutor(
-      backupTutor.filter((data) => {
+      instructors.filter((data) => {
         return (
           data.username.toLowerCase().includes(search.toLowerCase()) ||
           data.bio.toLowerCase().includes(search.toLowerCase())
         );
       })
     );
-  },[search])
-  const handleClickFilter=()=>{
-    setAllTutor(
-      backupTutor.filter((data) => {
-        if(startPrice=="" || endPrice=="")
-        {
-          return (
-            data
-            // (parseInt(data.hourlyWage) >=parseInt(startPrice) && parseInt(data.hourlyWage)<=parseInt(endPrice)) 
-            // &&
-            // data.bio.toLowerCase().includes(search.toLowerCase())
-          );
-        }
-        else
-        { return (
-          (parseInt(data.hourlyWage) >=parseInt(startPrice) && parseInt(data.hourlyWage)<=parseInt(endPrice)) 
-          // &&
-          // data.bio.toLowerCase().includes(search.toLowerCase())
-        );}
-       
-      })
-    );
-  }
+  }, [search]);
+  useEffect(()=>{
+    console.log(selectRatingChange)
+      setAllTutor(
+        instructors.filter((data) =>data.rating>=selectRatingChange)
+      );
+  },[selectRatingChange])
+  useEffect(()=>{
+    const backupinstructors=instructors;
+    if(selectOption==="Price hight to low")
+    {
+      setAllTutor(
+        backupinstructors.sort(function(a, b) {
+          return b.hourlyWage - a.hourlyWage;
+        })
+      );
+    }
+    else if(selectOption==="Price low to hight")
+    {
+      setAllTutor(
+        backupinstructors.sort(function(a, b) {
+          return a.hourlyWage - b.hourlyWage;
+        })
+      );
+    }
+  },[selectOption])
   return (
     <Layout>
       <div className="bg-[#f7f8fc] py-2">
         <div className="mx-5 mt-10 md:mx-10">
-          {/* <Search></Search> */}
-          {/* search */}
           <div>
             <div className="mt-2 flex flex-col justify-between md:flex-row">
-              <h3 className="text-2xl font-bold">46 search results found</h3>
+              <h3 className="text-2xl font-bold">{allTutor.length} search results found</h3>
               <div className="flex flex-row items-center justify-between gap-6">
-                <select className="border-0 p-2 text-xl">
-                  <option>Price hight to low</option>
-                  <option>Proce low to hight</option>
+                <select className="border-0 p-2 text-xl"
+                 value={selectOption}
+                 onChange={(e)=>{setSelectOption(e.target.value)}}>
+                  <option value={"Price hight to low"} >Price hight to low</option>
+                  <option value={"Price low to hight"}>Price low to hight</option>
                 </select>
                 <div className="flex flex-row gap-4">
                   <svg
@@ -119,14 +110,14 @@ export const FindInstructors = () => {
                     />
                   </svg>
                   <input
-                    className="w-full border-none"
+                    className="w-full border-none w-full"
                     type="text"
                     placeholder="What are you looking for?"
                     value={search}
-                    onChange={(e)=>setSearch(e.target.value)}
+                    onChange={(e) => setSearch(e.target.value)}
                   ></input>
                 </div>
-                <div className="flex-rowt flex items-center justify-start gap-4">
+                {/* <div className="flex-rowt flex items-center justify-start gap-4">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -144,10 +135,10 @@ export const FindInstructors = () => {
                   <select className="w-full border-0 ">
                     <option>Select category</option>
                   </select>
-                </div>
-                <a className="ml-1 rounded-xl bg-orange-500 p-2 text-center text-xl font-bold text-white">
+                </div> */}
+                {/* <a className="ml-1 rounded-xl bg-orange-500 p-2 text-center text-xl font-bold text-white">
                   Search now
-                </a>
+                </a> */}
               </div>
               <div className="ml-2 hidden w-[20%] flex-row items-center gap-4 md:flex">
                 <img
@@ -164,48 +155,20 @@ export const FindInstructors = () => {
               {/* <Sidebar /> */}
               <div className="p-5">
                 <div className="">
-                  {/* <div className="mt-6">
-          <details className="" open>
-            <summary className="text-xl font-bold">Subject & Level</summary>
-            <div className="mt-4">
-              <select className="w-full">
-                <option>ccxxxxxxxxx</option>
-                <option>ccxxxxxxxxx</option>
-                <option>ccxxxxxxxxx</option>
-              </select>
-            </div>
-          </details>
-        </div> */}
-                  <div className="mt-6">
-                    <details className="" open>
-                      <summary className="text-xl font-bold">
-                        Price range
-                      </summary>
-                      <div className="mt-4 flex flex-row justify-between">
-                        <input type="text w-[40%]" defaultValue={0} value={startPrice} onChange={(e)=>setStartPrice(e.target.value)} className="border border-black"/>
-                        <input type="text w-[40%]" defaultValue={1000} value={endPrice} onChange={(e)=>setEndPrice(e.target.value)} className="border border-black"/>
-                      </div>
-                    </details>
-                  </div>
-                  <div className="mt-6">
-                    <details className="" open>
-                      <summary className="text-xl font-bold">Gender</summary>
-                      <div className="mt-4 flex flex-row justify-between">
-                        <select className="w-full">
-                          <option>Both</option>
-                          <option>Male</option>
-                          <option>Female</option>
-                        </select>
-                      </div>
-                    </details>
-                  </div>
-
                   <div className="mt-6">
                     <details className="" open>
                       <summary className="text-xl font-bold">Rating</summary>
                       <div>
                         <div className="mt-4 flex flex-row items-center gap-4">
-                          <input type="checkbox" id="rate5" value={5} checked={rating5} onChange={()=>{setRating5(!rating5)}}/>
+                          <input
+                            type="radio"
+                            id="rate5"
+                            name="rating"
+                            value={5}
+                            onChange={(e) => {
+                              setSlectRatingChange(e.target.value);
+                            }}
+                          />
                           <label
                             htmlFor="rate5"
                             className="flex flex-row gap-1"
@@ -215,12 +178,19 @@ export const FindInstructors = () => {
                             <AiFillStar className="h-6 w-6 text-yellow-300" />
                             <AiFillStar className="h-6 w-6 text-yellow-300" />
                             <AiFillStar className="h-6 w-6 text-yellow-300" />
-                            <span>5.0</span>
-                            <span className="text-gray-200">/5.0</span>
+                            <span>5.0/5.0</span>
                           </label>
                         </div>
                         <div className="mt-4 flex flex-row items-center gap-4">
-                          <input type="checkbox" id="rate4" value={4}  checked={rating4} onChange={()=>{setRating4(!rating4)}}/>
+                          <input
+                            type="radio"
+                            id="rate4"
+                            name="rating"
+                            value={4}
+                            onChange={(e) => {
+                              setSlectRatingChange(e.target.value);
+                            }}
+                          />
                           <label
                             htmlFor="rate4"
                             className="flex flex-row gap-1"
@@ -230,12 +200,19 @@ export const FindInstructors = () => {
                             <AiFillStar className="h-6 w-6 text-yellow-300" />
                             <AiFillStar className="h-6 w-6 text-yellow-300" />
                             <AiFillStar className="h-6 w-6 text-gray-200" />
-                            <span>4.0</span>
-                            <span className="text-gray-200">/5.0</span>
+                            <span>Trở lên</span>
                           </label>
                         </div>
                         <div className="mt-4 flex flex-row items-center gap-4">
-                          <input type="checkbox" id="rate3" value={3}  checked={rating3} onChange={()=>{setRating3(!rating3)}}/>
+                          <input
+                            type="radio"
+                            id="rate3"
+                            name="rating"
+                            value={3}
+                            onChange={(e) => {
+                              setSlectRatingChange(e.target.value);
+                            }}
+                          />
                           <label
                             htmlFor="rate3"
                             className="flex flex-row gap-1"
@@ -245,14 +222,21 @@ export const FindInstructors = () => {
                             <AiFillStar className="h-6 w-6 text-yellow-300" />
                             <AiFillStar className="h-6 w-6 text-gray-200" />
                             <AiFillStar className="h-6 w-6 text-gray-200" />
-                            <span>3.0</span>
-                            <span className="text-gray-200">/5.0</span>
+                            <span>Trở lên</span>
                           </label>
                         </div>
                         <div className="mt-4 flex flex-row items-center gap-4">
-                          <input type="checkbox" id="rate5" value={2}  checked={rating2} onChange={()=>{setRating2(!rating2)}}/>
+                          <input
+                            type="radio"
+                            id="rate2"
+                            name="rating"
+                            value={2}
+                            onChange={(e) => {
+                              setSlectRatingChange(e.target.value);
+                            }}
+                          />
                           <label
-                            htmlFor="rate5"
+                            htmlFor="rate2"
                             className="flex flex-row gap-1"
                           >
                             <AiFillStar className="h-6 w-6 text-yellow-300" />
@@ -260,14 +244,21 @@ export const FindInstructors = () => {
                             <AiFillStar className="h-6 w-6 text-gray-200" />
                             <AiFillStar className="h-6 w-6 text-gray-200" />
                             <AiFillStar className="h-6 w-6 text-gray-200" />
-                            <span>2.0</span>
-                            <span className="text-gray-200">/5.0</span>
+                            <span>Trở lên</span>
                           </label>
                         </div>
                         <div className="mt-4 flex flex-row items-center gap-4">
-                          <input type="checkbox" id="rate5" value={5}  checked={rating1} onChange={()=>{setRating1(!rating1)}}/>
+                          <input
+                            type="radio"
+                            id="rate1"
+                            name="rating"
+                            value={1}
+                            onChange={(e) => {
+                              setSlectRatingChange(e.target.value);
+                            }}
+                          />
                           <label
-                            htmlFor="rate5"
+                            htmlFor="rate1"
                             className="flex flex-row gap-1"
                           >
                             <AiFillStar className="h-6 w-6 text-yellow-300" />
@@ -275,38 +266,29 @@ export const FindInstructors = () => {
                             <AiFillStar className="h-6 w-6 text-gray-200" />
                             <AiFillStar className="h-6 w-6 text-gray-200" />
                             <AiFillStar className="h-6 w-6 text-gray-200" />
-                            <span>1.0</span>
-                            <span className="text-gray-200">/5.0</span>
+                            <span>Trở lên</span>
                           </label>
                         </div>
                       </div>
                     </details>
                   </div>
-                  <div className="mt-6">
-                    <details className="" open>
-                      <summary className="text-xl font-bold">
-                        Tutor location
-                      </summary>
-                    </details>
-                  </div>
-                  <div className="mt-4 flex flex-col gap-4">
+                  {/* <div className="mt-4 flex flex-col gap-4">
                     <input
                       type="submit"
                       value={"Apply filters"}
-                      className="rounded-xl bg-red-500 p-2 text-white cursor-pointer"
+                      className="cursor-pointer rounded-xl bg-red-500 p-2 text-white"
                       onClick={handleClickFilter}
                     ></input>
-                   
-                  </div>
+                  </div> */}
                 </div>
               </div>
               {/*  */}
             </div>
             <div className="w-full shadow-lg lg:w-[70%]">
-              {allTutor&& allTutor.map((data,key)=>(
-                <Card_info data={data} key={key}/>
-              ))}
-              
+              {allTutor &&
+                allTutor.map((data, key) => (
+                  <Card_info data={data} key={key} />
+                ))}
             </div>
           </div>
         </div>
@@ -314,5 +296,16 @@ export const FindInstructors = () => {
     </Layout>
   );
 };
+
+export async function getServerSideProps() {
+  const instructors = await axios.get(
+    `${process.env.NEXT_PUBLIC_REST_API_ENDPOINT}/instructors`
+  );
+  return {
+    props: {
+      instructors: instructors.data,
+    },
+  };
+}
 
 export default FindInstructors;
